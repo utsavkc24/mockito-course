cdef905cd87d589fd52adff3daf6c76e184d28c8
Utsav Kumar <utsavkc24@gmail.com>
2022-01-29 20:26:39 +0530

    How to use BDD style

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
@@ -1,6 +1,9 @@
 package com.in28minutes.business;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,5 +26,23 @@ public class TodoBusinessImplMockitoTest {
 		List<String> todos = todoBusinessImpl
 				.retrieveTodosRelatedToSpring("Ranga");
 		assertEquals(2, todos.size());
+	}
+	
+	@Test
+	public void usingMockitoToRetrieveTodosRelatedToSpring_usingBDD() {
+		
+		//Given
+		TodoService todoService = mock(TodoService.class);
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+		given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
+		
+		//When
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Ranga");
+		
+		//Then
+		assertThat(todos.size(), is(2));
 	}
 }
