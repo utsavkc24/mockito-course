f5fa1a2787305f2bb018fe6147b4ec21370f457d
Utsav Kumar <utsavkc24@gmail.com>
2022-01-29 20:55:41 +0530

    verify() method

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
@@ -4,7 +4,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -21,10 +26,10 @@ public class TodoBusinessImplMockitoTest {
 		TodoService todoService = mock(TodoService.class);
 		List<String> allTodos = Arrays.asList("Learn Spring MVC",
 				"Learn Spring", "Learn to Dance");
-		when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+		when(todoService.retrieveTodos("Utsav")).thenReturn(allTodos);
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
 		List<String> todos = todoBusinessImpl
-				.retrieveTodosRelatedToSpring("Ranga");
+				.retrieveTodosRelatedToSpring("Utsav");
 		assertEquals(2, todos.size());
 	}
 	
@@ -35,14 +40,37 @@ public class TodoBusinessImplMockitoTest {
 		TodoService todoService = mock(TodoService.class);
 		List<String> allTodos = Arrays.asList("Learn Spring MVC",
 				"Learn Spring", "Learn to Dance");
-		given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
+		given(todoService.retrieveTodos("Utsav")).willReturn(allTodos);
 		
 		//When
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
 		List<String> todos = todoBusinessImpl
-				.retrieveTodosRelatedToSpring("Ranga");
+				.retrieveTodosRelatedToSpring("Utsav");
 		
 		//Then
 		assertThat(todos.size(), is(2));
+	}
+	
+	@Test
+	public void usingMockitoToDeleteTodosNotRelatedToSpring_usingBDD() {
+		
+		//Given
+		TodoService todoService = mock(TodoService.class);
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+		given(todoService.retrieveTodos("Utsav")).willReturn(allTodos);
+		
+		//When
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		 todoBusinessImpl.deleteTodosNotRelatedToSpring("Utsav");
+		
+		//Then
+		verify(todoService).deleteTodos("Learn to Dance"); // to verify method deleteTodos has executed
+		verify(todoService, never()).deleteTodos("Learn Spring"); // to verify method deleteTodos has not executed
+		verify(todoService, atLeastOnce()).deleteTodos("Learn to Dance"); // to verify method deleteTodos has executed atleast once
+		verify(todoService, atLeast(1)).deleteTodos("Learn to Dance"); // to verify method deleteTodos has executed atleast {number of arg pass}
+		verify(todoService, times(1)).deleteTodos("Learn to Dance"); // to verify method deleteTodos has executed {number of arg pass}
+
+
 	}
 }
